bdbf2192bb98075454bfcfea68caae15e8fc0156
SergioCainzo <sergiocainzo@gmail.com>
2024-11-24 10:13:38 -0300

    Instalando Bootstrap

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrincipalComponent } from "./principal/principal.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PrincipalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
