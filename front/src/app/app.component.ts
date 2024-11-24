061ab7533a21e4279e8bf98d452de2fe99316e27
SergioCainzo <sergiocainzo@gmail.com>
2024-11-24 10:10:52 -0300

    Iniciando Projeto

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PrincipalComponent } from "./principal/principal.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PrincipalComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'api-front';
+  title = 'front';
 }
