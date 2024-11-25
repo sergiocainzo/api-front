77b113fdd77c6044a0b64a0758755a352c94656d
SergioCainzo <sergiocainzo@gmail.com>
2024-11-24 23:08:12 -0300

    FormsModule e HttpClientModule

--- front/src/app/principal/principal.component.ts
+++ front/src/app/principal/principal.component.ts
@@ -1,15 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-principal',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css'
 })
 export class PrincipalComponent {
 
   // Variável para visibilidade dos botões
-  btnCadastro:boolean = true;
+  btnCadastro: boolean = true;
 
 
 
